4a3dd8889d7e460b99028b4357ba0e48ea4acc49
SaranyaOP <saranyaop98@gmail.com>
2025-12-06 11:11:12 +0530

    first commit-components created

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navlinks from './components/Navlinks'
+import Chatlist from './components/Chatlist'
+import Chatbox from './components/Chatbox'
+import Login from './components/Login'
+import Register from './components/Register'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Navlinks />
+      <Chatlist />
+      <Chatbox />
+      <Login /> */}
+      <Register />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='text-green-600'>Register</div>
+  )
+}
+
+export default Register
